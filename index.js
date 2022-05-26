b81ab8f4993b70a17154d81b61446aa55597ddd6
goforthandrock <59512395+goforthandrock@users.noreply.github.com>
2022-05-26 19:00:55 +0000

    revert to original

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+// Promise.all() with delays for each promise
 let tasks = [];
 for (let i = 0; i < 10; i++) {
-  const delay = 5000 * i;
+  const delay = 500 * i;
   tasks.push(new Promise(async function(resolve) {
     // the timer/delay
     await new Promise(res => setTimeout(res, delay));
@@ -8,11 +9,9 @@ for (let i = 0; i < 10; i++) {
     // the promise you want delayed
     // (for example):
     // let result = await axios.get(...);
-    let result = await new Promise(async r => {
-      //console.log("I'm the delayed promise"+delay+"...maybe an API call!");
-			fetch("https://api.stadtpuls.com/api/v3/sensors/91/records")
-      //throw "error"
-      return r; //result is delay ms for demo purposes
+    let result = await new Promise(r => {
+      console.log("I'm the delayed promise...maybe an API call!");
+      r(delay); //result is delay ms for demo purposes
     });
 
     //resolve outer/original promise with result
